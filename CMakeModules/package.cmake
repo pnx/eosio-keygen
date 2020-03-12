c5767e30fc33928c99732f16681f3ff0ecbe8eaf
Henrik Hautakoski <henrik@eossweden.org>
2020-03-12 15:22:46 +0100

    CMake: create a more generic way to build packages.

--- CMakeModules/package.cmake
+++ CMakeModules/package.cmake
@@ -1,65 +1,133 @@
 
 # --------------------------------
-#  Info
+#  Functions
 # --------------------------------
-set( PACKAGE_NAME ${PROJECT_NAME} )
-set( PACKAGE_DESCRIPTION ${PROJECT_DESCRIPTION} )
-set( PACKAGE_VERSION "${PROJECT_VERSION}" )
-set( PACKAGE_HOMEPAGE_URL ${PROJECT_HOMEPAGE_URL} )
-set( PACKAGE_MAINTAINER ${PROJECT_MAINTAINER})
 
-set( PACKAGE_PLATFORM "x86" )
+function(_set_default VAR_NAME DEFAULT_VALUE)
+
+	if(NOT ${VAR_NAME})
+		set( ${VAR_NAME} ${DEFAULT_VALUE} PARENT_SCOPE)
+	endif()
+endfunction()
+
+function(_set_fallback VAR_NAME)
+
+	set(var_list ${ARGN})
+
+	if(NOT ${VAR_NAME})
+		foreach (var IN LISTS var_list)
+			if (${var})
+				set( ${VAR_NAME} ${${var}} PARENT_SCOPE)
+				break()
+			endif()
+		endforeach()
+	endif()
+endfunction()
+
+function(add_package TARGET)
+
+	set(DEPENDANCIES ${ARGN})
+
+	add_dependencies(package ${TARGET})
+	if (DEPENDANCIES)
+		add_dependencies(${TARGET} ${DEPENDANCIES})
+	endif(DEPENDANCIES)
+endfunction()
+
+# --------------------------------
+#  Environment variables
+# --------------------------------
+
+# Define PACKAGE_ARCHITECTURE
+set( PACKAGE_ARCHITECTURE "x86" )
 if (CMAKE_SIZEOF_VOID_P EQUAL 8)
-	set( PACKAGE_PLATFORM "${PACKAGE_PLATFORM}_64" )
+	set( PACKAGE_ARCHITECTURE "${PACKAGE_ARCHITECTURE}_64" )
+endif()
+
+# Define PACKAGE_SYSTEM_NAME and PACKAGE_SYSTEM_VERSION
+if (UNIX)
+
+	# Try ubuntu.
+	execute_process(
+		COMMAND lsb_release -is
+		OUTPUT_VARIABLE PACKAGE_SYSTEM_NAME
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	string(TOLOWER "${PACKAGE_SYSTEM_NAME}" PACKAGE_SYSTEM_NAME )
+
+	if (PACKAGE_SYSTEM_NAME STREQUAL "ubuntu")
+
+		execute_process(
+			COMMAND lsb_release -rs
+			OUTPUT_VARIABLE PACKAGE_SYSTEM_VERSION
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+
+		# Debian uses different names for 32 and 64 bit.
+		if (PACKAGE_ARCHITECTURE EQUAL "x86")
+			set( PACKAGE_DEB_ARCHITECTURE "i386" )
+		else()
+			set( PACKAGE_DEB_ARCHITECTURE "amd64" )
+		endif()
+	endif()
+elseif (WIN32)
+	set(PACKAGE_SYSTEM_NAME "windows")
 endif()
 
 # --------------------------------
-#  Debian script
+#  Options
 # --------------------------------
 
-if (UNIX) # Only include in bash environments.
+option(PKG_TYPE "Type of package to build (zip, deb)" "zip")
 
-	execute_process(
-		COMMAND lsb_release -rs
-		OUTPUT_VARIABLE UBUNTU_VERSION
-	 	OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
+# --------------------------------
+#  Package definition
+# --------------------------------
 
-	# Debian uses different names for 32 and 64 bit.
-	if (PACKAGE_PLATFORM EQUAL "x86")
-		set( PACKAGE_DEB_PLATFORM "i386" )
-	else()
-		set( PACKAGE_DEB_PLATFORM "amd64" )
+set( PACKAGE_OUTPUT_DIR ${CMAKE_BINARY_DIR} )
+
+get_filename_component(_PACKAGE_TEMPLATE_DIR ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
+
+# Add dummy target for all packages.
+add_custom_target( package )
+
+# Define a package.
+function(package)
+
+	set( DEPENDANCIES ${ARGN} )
+
+	# --------------------------------
+	#  Info
+	# --------------------------------
+	set( PACKAGE_${PROJECT_NAME}_NAME ${PROJECT_NAME} )
+	_set_fallback( PACKAGE_${PROJECT_NAME}_DESCRIPTION PROJECT_DESCRIPTION CMAKE_PROJECT_DESCRIPTION )
+	_set_fallback( PACKAGE_${PROJECT_NAME}_VERSION PROJECT_VERSION CMAKE_PROJECT_VERSION )
+	_set_fallback( PACKAGE_${PROJECT_NAME}_HOMEPAGE_URL PROJECT_HOMEPAGE_URL CMAKE_PROJECT_HOMEPAGE_URL )
+	set( PACKAGE_${PROJECT_NAME}_MAINTAINER ${PROJECT_MAINTAINER})
+	_set_default( PACKAGE_${PROJECT_NAME}_ARCHITECTURE ${PACKAGE_ARCHITECTURE} )
+
+	_set_default( PACKAGE_${PROJECT_NAME}_FILENAME ${PACKAGE_${PROJECT_NAME}_NAME}_${PACKAGE_${PROJECT_NAME}_VERSION}-${PACKAGE_SYSTEM_NAME}-${PACKAGE_SYSTEM_VERSION}_${PACKAGE_${PROJECT_NAME}_ARCHITECTURE})
+
+
+	# --------------------------------
+	#  Debian
+	# --------------------------------
+	if (PKG_TYPE STREQUAL "deb")
+
+		if (NOT UNIX)
+			message(FATAL_ERROR "Can't build deb packages on this system")
+		endif()
+
+		include(${_PACKAGE_TEMPLATE_DIR}/package_deb.cmake)
+		add_package(${_PACKAGE_TARGET} ${DEPENDANCIES})
 	endif()
 
-	set( PACKAGE_DEB_RELEASE 1 CACHE STRING "Debian package release number")
-	set( PACKAGE_DEB_VERSION ${PACKAGE_VERSION}-${PACKAGE_DEB_RELEASE} )
-	set( PACKAGE_DEB_FILENAME ${PACKAGE_NAME}_${PACKAGE_DEB_VERSION}-ubuntu-${UBUNTU_VERSION}_${PACKAGE_DEB_PLATFORM}.deb )
-	set( DEB_ROOT "${CMAKE_CURRENT_BINARY_DIR}/debroot" )
+	# --------------------------------
+	#  ZIP
+	# --------------------------------
+	if (PKG_TYPE STREQUAL "zip")
+		include(${_PACKAGE_TEMPLATE_DIR}/package_zip.cmake)
+		add_package(${_PACKAGE_TARGET} ${DEPENDANCIES})
+	endif()
 
-	configure_file(debian_control.in ${DEB_ROOT}/DEBIAN/control @ONLY)
-
-	add_custom_target(package_deb
-		COMMAND ${CMAKE_COMMAND} --install . --prefix "${DEB_ROOT}${CMAKE_INSTALL_PREFIX}"
-		COMMAND fakeroot dpkg-deb --build ${DEB_ROOT} ${PACKAGE_DEB_FILENAME} > /dev/null
-		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-	)
-
-	add_dependencies(package_deb ${PROGRAM_EXE})
-endif (UNIX)
-
-# --------------------------------
-#  Windows
-# --------------------------------
-if (WIN32)
-
-	set ( PACKAGE_FILENAME "${PACKAGE_NAME}_${PACKAGE_VERSION}-windows-${PACKAGE_PLATFORM}" )
-
-	add_custom_target(package
-		COMMAND ${CMAKE_COMMAND} --install . --prefix "${PACKAGE_FILENAME}"
-		COMMAND ${CMAKE_COMMAND} -E tar c "${PACKAGE_FILENAME}.zip" --format=zip "${PACKAGE_FILENAME}"
-		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-	)
-
-	add_dependencies(package ${PROGRAM_EXE})
-endif (WIN32)
+endfunction()
---
+++ CMakeModules/package_deb.cmake
@@ -0,0 +1,50 @@
+
+# Write deb file
+function(_deb_write_control CONTROL)
+
+	set(fields
+		Package
+		Version
+		Architecture
+		Description
+		Maintainer
+		Homepage
+		Depends
+	)
+
+	# Write control file
+	file(WRITE ${CONTROL} "")
+	foreach(item IN LISTS fields)
+		string(TOUPPER "${item}" key )
+
+		if (PACKAGE_${PROJECT_NAME}_DEB_${key})
+			file(APPEND ${CONTROL} "${item}: ${PACKAGE_${PROJECT_NAME}_DEB_${key}}\n")
+		endif()
+
+	endforeach()
+
+endfunction()
+
+
+_set_default( PACKAGE_${PROJECT_NAME}_DEB_PACKAGE ${PACKAGE_${PROJECT_NAME}_NAME} )
+_set_default( PACKAGE_${PROJECT_NAME}_DEB_ARCHITECTURE ${PACKAGE_DEB_ARCHITECTURE} )
+_set_default( PACKAGE_${PROJECT_NAME}_DEB_DESCRIPTION ${PACKAGE_${PROJECT_NAME}_DESCRIPTION} )
+_set_default( PACKAGE_${PROJECT_NAME}_DEB_MAINTAINER ${PACKAGE_${PROJECT_NAME}_MAINTAINER} )
+_set_default( PACKAGE_${PROJECT_NAME}_DEB_HOMEPAGE ${PACKAGE_${PROJECT_NAME}_HOMEPAGE_URL} )
+set( PACKAGE_${PROJECT_NAME}_DEB_RELEASE 1 CACHE STRING "Debian package release number")
+_set_default( PACKAGE_${PROJECT_NAME}_DEB_VERSION ${PACKAGE_${PROJECT_NAME}_VERSION}-${PACKAGE_${PROJECT_NAME}_DEB_RELEASE} )
+_set_default( PACKAGE_${PROJECT_NAME}_DEB_FILENAME ${PROJECT_NAME}_${PACKAGE_${PROJECT_NAME}_DEB_VERSION}-${PACKAGE_SYSTEM_NAME}-${PACKAGE_SYSTEM_VERSION}_${PACKAGE_${PROJECT_NAME}_DEB_ARCHITECTURE}.deb )
+_set_default( PACKAGE_${PROJECT_NAME}_DEB_ROOT "${PACKAGE_OUTPUT_DIR}/pack/deb/${PROJECT_NAME}/debroot" )
+
+# Write control file
+_deb_write_control( ${PACKAGE_${PROJECT_NAME}_DEB_ROOT}/DEBIAN/control )
+
+set(_PACKAGE_TARGET package_${PROJECT_NAME}_deb)
+
+# Add target
+add_custom_target(${_PACKAGE_TARGET}
+	COMMENT "Creating deb package: ${PACKAGE_${PROJECT_NAME}_DEB_FILENAME}"
+	COMMAND ${CMAKE_COMMAND} --install . --component ${PROJECT_NAME} --prefix "${PACKAGE_${PROJECT_NAME}_DEB_ROOT}${CMAKE_INSTALL_PREFIX}"
+	COMMAND fakeroot dpkg-deb --build ${PACKAGE_${PROJECT_NAME}_DEB_ROOT} ${PACKAGE_${PROJECT_NAME}_DEB_FILENAME} > /dev/null
+	WORKING_DIRECTORY ${PACKAGE_OUTPUT_DIR}
+)
